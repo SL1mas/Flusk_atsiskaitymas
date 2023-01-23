c4dc1be4c662720981d069ca0198d919ebef2eb6
SL1mas <sslimass@gmail.com>
2023-01-23 14:56:16 +0200

    3rd commit (added bills and groups page styles, modified login in register pages, created groups database)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,52 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+# from seed_groups_data import Group
+
+base_dir = os.path.dirname(__file__)
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(base_dir, 'data2.db')}"
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, user_id, name, email, password):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, group_id, name):
+        self.group_id = group_id
+        self.name = name
+
+
+class Bill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, bill_id, description, amount):
+        self.bill_id = bill_id
+        self.description = description
+        self.amount = amount
 
 
 @app.route("/login")
@@ -15,12 +61,34 @@ def register():
 
 @app.route("/bills")
 def bills():
-    return render_template("bills.html")
+    bills = Bill.query.all()
+    return render_template("bills.html", bills=bills)
+
+
+# @app.route("/bills/<int:id>")
+# def bills(id):
+#     bills = Bill.query.get(id)
+#     return render_template("bills.html", bills=bills)
 
 
 @app.route("/groups")
 def groups():
-    return render_template("groups.html")
+    groups = Group.query.all()
+    return render_template("groups.html", groups=groups)
+
+# @app.route('/article/add', methods=['GET', 'POST'])
+# def add_article():
+#     form = ArticleForm(request.form)
+#     if request.method == 'POST' and form.validate():
+#         article = Articles(form.author.data.id,
+#                            form.title.data, form.text.data)
+#         for tag in form.tags.data:
+#             article.tags.append(Tags.query.get(tag.id))
+#         db.session.add(article)
+#         db.session.commit()
+#         flash('Forma išsaugota sėkmingai')
+#         return redirect('/')
+#     return render_template('addArticle.html', form=form)
 
 
 if __name__ == "__main__":
---
+++ seed_groups_data.py
@@ -0,0 +1,20 @@
+from __init__ import db, Group, Bill
+
+group1 = Group("1", "Trip to Wonderland")
+group2 = Group("2", "Trip to Latvia")
+group3 = Group("3", "Trip to Estija")
+group4 = Group("4", "Trip to Space")
+group5 = Group("5", "Trip to Mountains")
+group6 = Group("6", "Trip to USA")
+group7 = Group("12", "Trip to Waterfall")
+group8 = Group("20", "Trip to Island")
+group9 = Group("129", "Trip to Nowhere")
+group10 = Group("1000", "Trip to Disneyland")
+
+bill1 = Bill("1", "Bilietas", "10")
+bill2 = Bill("2", "Skrydis", "150")
+
+db.create_all()
+db.session.add_all([group1, group2, group3, group4, group5,
+                   group6, group7, group8, group9, group10, bill1, bill2])
+db.session.commit()
